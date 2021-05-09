59b6b6cfad9b0a27b672ae09fc7d9dba70412ad0
jaydipbhesaniya <jaydipbhesaniya009@gmail.com>
2021-05-09 13:47:30 -0400

    header for variant included

--- hello.c
+++ hello.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "variant.h"
 int main()
 {
  printf("maruti\n");
  printf("Jay Hanuman\n");
- return 0;
+ 
  const char* get_variant(uint8 variant_number)
  {
 	 // function will pick string from here according to number passed 
  }
  printf("variant %d:\n", get_variant()); // added this line 
+ return 0;
+ 
 }
